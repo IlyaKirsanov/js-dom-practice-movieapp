9b15626d00d3991dea233982bf79f1522a2ead4b
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-04-22 12:24:02 +0300

    cancelAddMovieButton selecting, event closing modal and backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieModal = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
@@ -13,7 +14,12 @@ const backdropToggle = () => {
 
 const backdropClickHandler = () =>{
     toggleMovieModal();
-}
+};
+
+const cancelAddMovie = () =>{
+    toggleMovieModal();
+};
 
 startAddMovieModal.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
