0a92958ff4f1370fd8b49771836a218b1e0fbf48
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-04-22 12:20:04 +0300

    Selecting and open backdrop, closing backdrop toggling CSS classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,19 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieModal = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
 
-const toggleMovieModal = () =>{
+const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+    backdropToggle();
 };
 
+const backdropToggle = () => {
+    backdrop.classList.toggle('visible');
+};
+
+const backdropClickHandler = () =>{
+    toggleMovieModal();
+}
+
 startAddMovieModal.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
