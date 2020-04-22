0eaf6718960bb4d7c6b0faeecbbd970c87a3df21
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-04-22 13:34:23 +0300

    Deleting Movie Elements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -16,7 +16,21 @@ const updateUI = () => {
     }
 };
 
-const renderNewMovieElement = (title, imageUrl, rating) => {
+const deleteMovieHandler= (movieId) =>{
+    let movieIndex = 0;
+    for(const movie of movies) {
+        if(movie.id === movieId){
+            break;
+        }
+        movieIndex++;
+    }
+    movies.splice(movieIndex,1);
+    const listRoot = document.getElementById('movie-list');
+    listRoot.children[movieIndex].remove();
+    //listRoot.removeChild(listRoot.children[movieIndex]);
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
     const newMovieElement = document.createElement('li');
     newMovieElement.className = 'movie-element';
     newMovieElement.innerHTML = `
@@ -28,9 +42,10 @@ const renderNewMovieElement = (title, imageUrl, rating) => {
           <p>${rating}/5 stars</p>
         </div>
     `;
-
+    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id));
     const listRoot = document.getElementById('movie-list');
-    listRoot.append(newMovieElement)
+    listRoot.append(newMovieElement);
+
 };
 
 const toggleMovieModal = () => {
@@ -71,6 +86,7 @@ const addMovieHandler = () => {
     }
 
     const newMovie = {
+        id: Math.random().toString(),
         title: titleValue,
         image: imageUrlValue,
         rating: ratingValue
@@ -79,7 +95,7 @@ const addMovieHandler = () => {
     movies.push(newMovie);
     console.log(movies);
     toggleMovieModal();
-    renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+    renderNewMovieElement(newMovie.id, newMovie.title, newMovie.image, newMovie.rating);
     clearMovieInput();
     updateUI();
 };
