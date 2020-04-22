db41436a0c834234c5da706c916ffa60ac234c43
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-04-22 13:54:46 +0300

    Confirmation logic of deletion process

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,7 +17,7 @@ const updateUI = () => {
     }
 };
 
-const deleteMovie = (movieId) => {
+const deleteMovieHandler = (movieId) => {
     let movieIndex = 0;
     for(const movie of movies) {
         if(movie.id === movieId){
@@ -28,6 +28,7 @@ const deleteMovie = (movieId) => {
     movies.splice(movieIndex,1);
     const listRoot = document.getElementById('movie-list');
     listRoot.children[movieIndex].remove();
+    backdropToggle();
     //listRoot.removeChild(listRoot.children[movieIndex]);
 };
 
@@ -37,11 +38,16 @@ const closeMovieDeletionModal = () =>{
 
 };
 
-const deleteMovieHandler= (movieId) =>{
+const startDeleteMovieHandler= (movieId) =>{
 
     deleteMovieModal.classList.add('visible');
     backdropToggle();
-    //deleteMovie(movieId);
+    const cancelDeletionButton = deleteMovieModal.querySelector('.btn--passive');
+    const confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
+
+    cancelDeletionButton.addEventListener('click', closeMovieDeletionModal);
+    confirmDeletionButton.addEventListener('click', deleteMovieHandler.bind(null, movieId));
+
 };
 
 const renderNewMovieElement = (id, title, imageUrl, rating) => {
@@ -56,7 +62,7 @@ const renderNewMovieElement = (id, title, imageUrl, rating) => {
           <p>${rating}/5 stars</p>
         </div>
     `;
-    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id));
+    newMovieElement.addEventListener('click', startDeleteMovieHandler.bind(null, id));
     const listRoot = document.getElementById('movie-list');
     listRoot.append(newMovieElement);
 
@@ -64,7 +70,7 @@ const renderNewMovieElement = (id, title, imageUrl, rating) => {
 
 const closeMovieModal = () =>{
     addMovieModal.classList.remove('visible');
-}
+};
 
 const showMovieModal = () => {
     addMovieModal.classList.add('visible');
